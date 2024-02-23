bba57536facd5c9364d877cb222572a80dc83709
Douglas R <douglas.srochak@gmail.com>
2024-02-23 18:46:16 -0300

    feat: Add search and nearby functionality to GymsRepository

--- 03-api-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
+++ 03-api-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,7 +1,8 @@
 import { Gym, Prisma } from '@prisma/client';
 
-import { GymsRepository } from '../gyms-repository';
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository';
 import { randomUUID } from 'crypto';
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 
 export class InMemoryGymsRepository implements GymsRepository {
   private static instance: InMemoryGymsRepository;
@@ -16,6 +17,10 @@ export class InMemoryGymsRepository implements GymsRepository {
       InMemoryGymsRepository.instance = new InMemoryGymsRepository();
     }
     return InMemoryGymsRepository.instance;
+  }
+
+  clear() {
+    this.#gymsRepository = [];
   }
 
   async create(data: Prisma.GymCreateInput) {
@@ -36,5 +41,33 @@ export class InMemoryGymsRepository implements GymsRepository {
   async findById(id: string): Promise<Gym | null> {
     const gym = this.#gymsRepository.find((gym) => gym.id === id);
     return gym ?? null;
+  }
+
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    const MAX_ITEMS_PER_PAGE = 20;
+    return this.#gymsRepository
+      .filter((gym) => gym.title.includes(query))
+      .slice((page - 1) * MAX_ITEMS_PER_PAGE, page * MAX_ITEMS_PER_PAGE);
+  }
+
+  async findManyNearby({
+    latitude,
+    longitude,
+    page,
+  }: FindManyNearbyParams): Promise<Gym[]> {
+    const MAX_ITEMS_PER_PAGE = 20;
+    const MAX_DISTANCE = 10; // Kilometers
+    return this.#gymsRepository
+      .filter((gym) => {
+        const distance = getDistanceBetweenCoordinates(
+          {
+            latitude: gym.latitude.toNumber(),
+            longitude: gym.longitude.toNumber(),
+          },
+          { latitude, longitude }
+        );
+        return distance < MAX_DISTANCE;
+      })
+      .slice((page - 1) * MAX_ITEMS_PER_PAGE, page * MAX_ITEMS_PER_PAGE);
   }
 }
