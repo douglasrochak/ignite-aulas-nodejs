d4b6e25cfb0d50a176343e6c8bad4f4139e80388
Douglas R <douglas.srochak@gmail.com>
2024-02-26 05:39:54 -0300

    test: Update authentication method in test files

--- 03-api-solid/src/http/controllers/check-ins/validate.spec.ts
+++ 03-api-solid/src/http/controllers/check-ins/validate.spec.ts
@@ -16,7 +16,7 @@ describe('Validate Controller', () => {
   });
 
   it('Should be able to validate a check-in', async () => {
-    const { token } = await createAndAuthenticateUser(app);
+    const { token } = await createAndAuthenticateUser(app, true);
 
     const user = await prisma.user.findFirstOrThrow();
     const gym = await prisma.gym.create({
