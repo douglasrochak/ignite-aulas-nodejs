5400970e27c789d82280fcbaf91f6729d886f1ee
Douglas R <douglas.srochak@gmail.com>
2024-02-26 05:39:32 -0300

    feat: Add role-based authorization to check-in and gym creation routes

--- 03-api-solid/src/http/controllers/check-ins/routes.ts
+++ 03-api-solid/src/http/controllers/check-ins/routes.ts
@@ -5,13 +5,18 @@ import { create } from './create.controller';
 import { history } from './history.controller';
 import { validate } from './validate.controller';
 import { metrics } from './metrics.controller';
+import { verifyUserRole } from '@/http/middlewares/verify-user-role';
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT);
 
   app.post('/gyms/:gymId/check-ins', create);
 
-  app.patch('/check-ins/:checkInId/validate', validate);
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: verifyUserRole('ADMIN') },
+    validate
+  );
 
   app.get('/gyms/metrics', metrics);
   app.get('/gyms/check-ins', history);
--- 03-api-solid/src/http/controllers/gyms/routes.ts
+++ 03-api-solid/src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { FastifyInstance } from 'fastify';
 import { nearby } from './nearby.controller';
 import { search } from './search.controller';
 import { create } from './create.controller';
+import { verifyUserRole } from '@/http/middlewares/verify-user-role';
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT);
 
-  app.post('/gyms', create);
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create);
 
   app.get('/gyms/search', search);
   app.get('/gyms/nearby', nearby);
