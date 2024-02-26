7704ab9489ecbb6ff7761dda1b8a8bb14439a4a8
Douglas R <douglas.srochak@gmail.com>
2024-02-26 05:37:37 -0300

    feat: Add role property to user interface in fastify-jwt.d.ts

--- 03-api-solid/src/@types/fastify-jwt.d.ts
+++ 03-api-solid/src/@types/fastify-jwt.d.ts
@@ -3,7 +3,8 @@ import '@fastify/jwt';
 declare module '@fastify/jwt' {
   interface FastifyJWT {
     user: {
+      role: MEMBER | ADMIN;
       sub: string;
-    }; // user type is return type of `request.user` object
+    };
   }
 }
